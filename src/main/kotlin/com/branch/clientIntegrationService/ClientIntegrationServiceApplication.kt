b6925c99f2bbb7719d4cd20d1b44ab78c973874e
privacystatement1 <116470640+privacystatement1@users.noreply.github.com>
2022-10-22 21:54:02 -0400

    Create ClientIntegrationServiceApplication.kt

---
+++ src/main/kotlin/com/branch/clientIntegrationService/ClientIntegrationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.branch.clientIntegrationService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientIntegrationServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientIntegrationServiceApplication>(*args)
+}
