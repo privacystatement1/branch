da3bd8da5d1e50fd15cf732305d18afd8b7c16ae
privacystatement1 <116470640+privacystatement1@users.noreply.github.com>
2022-10-22 22:10:13 -0400

    Create ClientIntegrationServiceApplicationTests.kt

---
+++ src/test/kotlin/com/branch/clientIntegrationService/ClientIntegrationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.branch.clientIntegrationService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientIntegrationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
